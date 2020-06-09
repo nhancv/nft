f42f010ade185cb034e848e25bb5f0a46fc82f28
Nhan Cao <nhancv92@gmail.com>
2020-06-09 15:01:11 +0700

    update app style

--- lib/utils/app_style.dart
+++ lib/utils/app_style.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/cupertino.dart';
 
-TextStyle boldTextStyle(double size, Color color) => TextStyle(
-      fontSize: size,
-      fontWeight: FontWeight.bold,
-      color: color,
-    );
+TextStyle boldTextStyle(double size, Color color, {double height}) => TextStyle(
+  fontSize: size,
+  fontWeight: FontWeight.bold,
+  color: color,
+  height: height,
+);
 
-TextStyle mediumTextStyle(double size, Color color) => TextStyle(
+TextStyle mediumTextStyle(double size, Color color, {double height}) =>
+    TextStyle(
       fontSize: size,
       fontWeight: FontWeight.w500,
       color: color,
+      height: height,
     );
 
-TextStyle normalTextStyle(double size, Color color, {double height = 1.2}) =>
+TextStyle normalTextStyle(double size, Color color, {double height = 1.1}) =>
     TextStyle(
-        fontSize: size,
-        fontWeight: FontWeight.normal,
-        color: color,
-        height: height);
+      fontSize: size,
+      fontWeight: FontWeight.normal,
+      color: color,
+      height: height,
+    );
 
-TextStyle lightTextStyle(double size, Color color) => TextStyle(
+TextStyle lightTextStyle(double size, Color color, {double height}) =>
+    TextStyle(
       fontSize: size,
       fontWeight: FontWeight.w300,
       color: color,
+      height: height,
     );
