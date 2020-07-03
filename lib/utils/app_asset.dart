dad4cb85c29d91e5cd1a861eced3e6b6d2c68f04
Nhan Cao <nhancv92@gmail.com>
2020-07-03 08:44:59 +0700

    feat: update assets

--- lib/utils/app_asset.dart
+++ lib/utils/app_asset.dart
@@ -1,13 +1,5 @@
 class AppImages {
-  static const icBack = 'assets/base/icons/ic_back.png';
   static const icAdd = 'assets/base/icons/ic_add.png';
-  static const icAddMail = 'assets/base/icons/ic_add_mail.png';
-  static const icCamera = 'assets/base/icons/ic_camera.png';
-  static const icHeart = 'assets/base/icons/ic_heart.png';
-  static const icMore = 'assets/base/icons/ic_more.png';
-  static const icNoti = 'assets/base/icons/ic_noti.png';
-  static const icEdit = 'assets/base/icons/ic_edit.png';
-  static const icSearch = 'assets/base/icons/ic_search.png';
   static const icWarning = 'assets/base/icons/ic_warning.png';
   static const icEmpty = 'assets/base/icons/ic_empty.png';
   static const icAppIcon = 'assets/base/icons/app_icon.png';
