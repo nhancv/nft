34a5bd71f27adf322f3a7ea2c232581b7abd6f5d
Nhan Cao <nhancv92@gmail.com>
2020-07-03 09:07:13 +0700

    feat: update theme

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -4,25 +4,21 @@ import 'package:provider/provider.dart';
 
 class AppTheme {
   final bool isDark;
-  final Color headerBgColor;
   final Color backgroundColor;
 
   const AppTheme({
     @required this.isDark,
     @required this.backgroundColor,
-    @required this.headerBgColor,
   });
 
   const AppTheme.dark({
     this.isDark = true,
     this.backgroundColor = Colors.black,
-    this.headerBgColor = const Color(0xFF191819),
   });
 
   const AppTheme.light({
     this.isDark = false,
     this.backgroundColor = Colors.white,
-    this.headerBgColor = const Color(0xFFF8F7F7),
   });
 }
 
@@ -33,7 +29,6 @@ class AppThemeProvider with ChangeNotifier {
     theme = appTheme;
     notifyListeners();
   }
-
 }
 
 extension AppThemeExt on BuildContext {
