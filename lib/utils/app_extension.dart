7f23d16f4440ba02dfa741cd9f6f42ace53ff592
Nhan Cao <nhancv92@gmail.com>
2020-07-15 14:18:40 +0700

    add app extension

---
+++ lib/utils/app_extension.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+extension SizeExtension on num {
+  double get W => w.toDouble();
+
+  double get H => h.toDouble();
+
+  // ignore: non_constant_identifier_names
+  double get SP => sp.toDouble();
+
+  // ignore: non_constant_identifier_names
+  double get SSP => ssp.toDouble();
+}
