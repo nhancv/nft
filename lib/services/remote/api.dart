62f1327f5d91153a69c6efe4a6599a01134b5080
Nhan Cao <nhancv92@gmail.com>
2020-07-16 13:49:13 +0700

    chore: update wrap e

--- lib/services/remote/api.dart
+++ lib/services/remote/api.dart
@@ -39,13 +39,12 @@ class Api {
     try {
       return await dioApi();
     } catch (error) {
-      String errorMessage = error.toString();
       if (error is DioError && error.type == DioErrorType.RESPONSE) {
         final Response<dynamic> response = error.response;
         /// if you want by pass dio header error code to get response content
         /// just uncomment line below
         //return response;
-        errorMessage =
+        final String errorMessage =
             'Code ${response.statusCode} - ${response.statusMessage} ${response.data != null ? '\n' : ''} ${response.data}';
         throw DioError(
             request: error.request,
