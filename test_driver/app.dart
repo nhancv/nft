0ec335c410b7d1a5611564d538a380c7097dad3a
Nhan Cao <nhancv92@gmail.com>
2020-10-15 11:01:55 +0700

    chore: update docs

--- test_driver/app.dart
+++ test_driver/app.dart
@@ -3,6 +3,7 @@ import 'package:nft/main.dart' as app;
 
 /// https://flutter.dev/docs/cookbook/testing/integration/introduction
 /// Run integration test: flutter drive --target=test_driver/app.dart
+/// flutter drive --target=test_driver/app.dart --driver=test_driver/app_test.dart
 void main() {
   // This line enables the extension.
   enableFlutterDriverExtension();
