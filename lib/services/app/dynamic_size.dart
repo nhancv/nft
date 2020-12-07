ef1c9782b4e1bfd89ee3984fec945a2e1d8df152
Nhan Cao <nhancv92@gmail.com>
2020-12-07 21:42:13 +0700

    feat: update ios screen size

--- lib/services/app/dynamic_size.dart
+++ lib/services/app/dynamic_size.dart
@@ -14,7 +14,16 @@ mixin DynamicSize {
   void initDynamicSize(BuildContext context) {
     /// Set the fit size (fill in the screen size of the device in the design)
     /// https://www.paintcodeapp.com/news/ultimate-guide-to-iphone-resolutions
-    /// Size of iPhone 8: 375 × 667 (points) - 750 × 1334 (pixels) (2x)
+    /// iPhone 2, 3, 4, 4s                => 3.5": 320 x 480 (points)
+    /// iPhone 5, 5s, 5C, SE (1st Gen)    => 4": 320 × 568 (points)
+    /// iPhone 6, 6s, 7, 8, SE (2st Gen)  => 4.7": 375 x 667 (points)
+    /// iPhone 6+, 6s+, 7+, 8+            => 5.5": 414 x 736 (points)
+    /// iPhone 11Pro, X, Xs               => 5.8": 375 x 812 (points)
+    /// iPhone 11, Xr                     => 6.1": 414 × 896 (points)
+    /// iPhone 11Pro Max, Xs Max          => 6.5": 414 x 896 (points)
+    /// iPhone 12 mini                    => 5.4": 375 x 812 (points)
+    /// iPhone 12, 12 Pro                 => 6.1": 390 x 844 (points)
+    /// iPhone 12 Pro Max                 => 6.7": 428 x 926 (points)
     ScreenUtil.init(context, designSize: const Size(375, 667));
   }
 }
