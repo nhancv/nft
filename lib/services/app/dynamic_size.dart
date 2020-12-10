458be0f8a7212da3bc1599385f8cafc0de909471
Nhan Cao <nhancv92@gmail.com>
2020-12-10 22:02:05 +0700

    feat: update dynamic size

--- lib/services/app/dynamic_size.dart
+++ lib/services/app/dynamic_size.dart
@@ -10,8 +10,21 @@ import 'package:flutter_screenutil/screenutil.dart';
 ///   }
 /// }
 mixin DynamicSize {
+
   /// Init dynamic size
-  void initDynamicSize(BuildContext context, {double w, double h}) {
+  //     ScreenUtil().pixelRatio       //Device pixel density
+  //     ScreenUtil().screenWidth   (sdk>=2.6 : 1.sw)    //Device width
+  //     ScreenUtil().screenHeight  (sdk>=2.6 : 1.sh)    //Device height
+  //     ScreenUtil().bottomBarHeight  //Bottom safe zone distance, suitable for buttons with full screen
+  //     ScreenUtil().statusBarHeight  //Status bar height , Notch will be higher Unit px
+  //     ScreenUtil().textScaleFactor  //System font scaling factor
+  //
+  //     ScreenUtil().scaleWidth //Ratio of actual width dp to design draft px
+  //     ScreenUtil().scaleHeight //Ratio of actual height dp to design draft px
+  //
+  //     0.2.sw  //0.2 times the screen width
+  //     0.5.sh  //50% of screen height
+  void initDynamicSize(BuildContext context) {
     /// Set the fit size (fill in the screen size of the device in the design)
     /// https://www.paintcodeapp.com/news/ultimate-guide-to-iphone-resolutions
     /// iPhone 2, 3, 4, 4s                => 3.5": 320 x 480 (points)
@@ -24,6 +37,6 @@ mixin DynamicSize {
     /// iPhone 12 mini                    => 5.4": 375 x 812 (points)
     /// iPhone 12, 12 Pro                 => 6.1": 390 x 844 (points)
     /// iPhone 12 Pro Max                 => 6.7": 428 x 926 (points)
-    ScreenUtil.init(context, designSize: Size(w ?? 375, h ?? 667));
+    ScreenUtil.init(context, designSize: const Size(375, 812));
   }
 }
--- lib/utils/app_extension.dart
+++ lib/utils/app_extension.dart
@@ -1,16 +1,58 @@
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
 
 class AppExtension {}
 
 /// Extension for screen util
 extension SizeExtension on num {
+  // setWidth
   double get W => w.toDouble();
 
+  // setHeight
   double get H => h.toDouble();
 
+  // Set sp with default allowFontScalingSelf = false
   /// ignore: non_constant_identifier_names
   double get SP => sp.toDouble();
 
+  // Set sp with allowFontScalingSelf: true
   /// ignore: non_constant_identifier_names
   double get SSP => ssp.toDouble();
+
+  // Set sp with allowFontScalingSelf: false
+  /// ignore: non_constant_identifier_names
+  double get NSP => nsp.toDouble();
+
+  // % of screen width
+  // ignore: non_constant_identifier_names
+  double get SW => sw.toDouble();
+
+  // % of screen height
+  // ignore: non_constant_identifier_names
+  double get SH => sh.toDouble();
+
+}
+
+/// Extension for DateTime
+extension DateTimeExtension on DateTime {
+  // Convert DateTime to String
+  // 2020-04-03T11:57:00
+  String toDateTimeString() {
+    return DateFormat('yyyy-MM-ddThh:mm:ss').format(this);
+  }
+}
+
+/// Extension for DateTime from String
+extension DateTimeStringExtendsion on String {
+  // Check Null
+  bool get isNull => this == null;
+
+  // Check Null or Empty
+  bool get isNullOrEmpty => isNull || isEmpty;
+
+  // Convert to DateTime by pattern
+  DateTime toDateTime(String pattern) {
+    return isNullOrEmpty ? null : DateFormat(pattern).parse(this);
+  }
+
 }
