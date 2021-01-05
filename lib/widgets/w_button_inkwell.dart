c83f73a0a1651be85ffafb3c13ed02d207d67404
Nhan Cao <nhancv92@gmail.com>
2021-01-05 23:40:24 +0700

    feat: button inkwell

---
+++ lib/widgets/w_button_inkwell.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class WButtonInkwell extends StatelessWidget {
+  const WButtonInkwell(
+      {Key key, this.splashColor, this.onTap, this.borderRadius, this.child})
+      : super(key: key);
+
+  final Color splashColor;
+  final Widget child;
+  final Function() onTap;
+  final BorderRadius borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        borderRadius: borderRadius,
+        splashColor: splashColor,
+        child: child,
+        onTap: onTap,
+      ),
+    );
+  }
+}
