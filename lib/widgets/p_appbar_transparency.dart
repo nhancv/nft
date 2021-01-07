b063bd21d18c457cedf4357f6e76220fd260b965
Nhan Cao <nhancv92@gmail.com>
2021-01-07 13:07:28 +0700

    feat: update app bar transparent

--- lib/widgets/p_appbar_transparency.dart
+++ lib/widgets/p_appbar_transparency.dart
@@ -4,15 +4,23 @@ import 'package:nft/utils/app_theme.dart';
 import 'package:nft/widgets/p_material.dart';
 
 class PAppBarTransparency extends StatelessWidget {
-  const PAppBarTransparency({@required this.child, Key key}) : super(key: key);
+  const PAppBarTransparency(
+      {@required this.child, this.forceStatusIconLight, Key key})
+      : super(key: key);
 
   final Widget child;
+  final bool forceStatusIconLight;
 
   @override
   Widget build(BuildContext context) {
     final AppTheme theme = context.theme();
-    final SystemUiOverlayStyle uiOverlayStyle =
-        theme.isDark ? SystemUiOverlayStyle.dark : SystemUiOverlayStyle.light;
+    final SystemUiOverlayStyle uiOverlayStyle = forceStatusIconLight == null
+        ? (theme.isDark
+            ? SystemUiOverlayStyle.dark
+            : SystemUiOverlayStyle.light)
+        : forceStatusIconLight == true
+            ? SystemUiOverlayStyle.light
+            : SystemUiOverlayStyle.dark;
     return PMaterial(
       child: AnnotatedRegion<SystemUiOverlayStyle>(
         value: uiOverlayStyle.copyWith(statusBarColor: Colors.transparent),
