d2c84dd0ce4201d977c697bb8c62e90fe86b001c
Nhan Cao <nhancv92@gmail.com>
2021-01-15 18:47:05 +0700

    feat: update behide keyboard

--- lib/widgets/w_behide_keyboard.dart
+++ lib/widgets/w_behide_keyboard.dart
@@ -2,11 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:nft/utils/app_route.dart';
 
 class WBehindKeyboard extends StatefulWidget {
-  const WBehindKeyboard({Key key, @required this.child, this.reversed = false})
+  const WBehindKeyboard(
+      {Key key,
+        @required this.child,
+        this.reversed = false,
+        this.keepView = true})
       : super(key: key);
 
   final Widget child;
+
+  /// As default, view hide when keyboard opened.
+  /// Set reversed = true, it'll reverse the logic.
   final bool reversed;
+
+  /// if you want hide view but keep the layout, set keepView = true as default
+  /// otherwise a view will dispose when keyboard opened
+  /// Both mode support touch through below widget as well
+  final bool keepView;
 
   @override
   _WBehindKeyboardState createState() => _WBehindKeyboardState();
@@ -19,9 +31,13 @@ class _WBehindKeyboardState extends State<WBehindKeyboard> {
   Widget build(BuildContext context) {
     _keyboardVisible =
         MediaQuery.of(AppRoute.I.appContext).viewInsets.bottom != 0;
-    return Opacity(
-      opacity: (widget.reversed ? !_keyboardVisible : _keyboardVisible) ? 0 : 1,
+    final bool visible =
+    !(widget.reversed ? !_keyboardVisible : _keyboardVisible);
+    return widget.keepView
+        ? Opacity(
+      opacity: visible ? 1 : 0,
       child: widget.child,
-    );
+    )
+        : Visibility(child: widget.child, visible: visible);
   }
 }
