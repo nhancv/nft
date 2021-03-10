f3bba7621902fae773296ab8ca2950129980118f
Nhan Cao <nhancv92@gmail.com>
2021-03-10 18:12:18 +0700

    feat: update wenv

--- lib/widgets/w_env.dart
+++ lib/widgets/w_env.dart
@@ -2,8 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:nft/services/safety/base_stateless.dart';
 import 'package:nft/utils/app_config.dart';
-import 'package:nft/utils/app_extension.dart';
-import 'package:nft/utils/app_style.dart';
+import 'package:nft/widgets/w_rounded_text.dart';
 
 // ignore: must_be_immutable
 class WEnv extends BaseStateless {
@@ -12,19 +11,7 @@ class WEnv extends BaseStateless {
     super.build(context);
     final Env env = AppConfig.I.env;
     return env.envType != EnvType.prod
-        ? Container(
-            padding: EdgeInsets.all(2.W),
-            margin: EdgeInsets.symmetric(horizontal: 5.W),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.red),
-              borderRadius: BorderRadius.circular(10.W),
-            ),
-            child: Text(
-              describeEnum(env.envType),
-              textAlign: TextAlign.center,
-              style: boldTextStyle(15.SP, color: Colors.red),
-            ),
-          )
+        ? WRoundedText(text: describeEnum(env.envType), color: Colors.red)
         : Container();
   }
 }
