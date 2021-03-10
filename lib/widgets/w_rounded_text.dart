4187c6294bddd1e03fe52c7a4d2bea6d49954ceb
Nhan Cao <nhancv92@gmail.com>
2021-03-10 18:09:54 +0700

    feat: add rounded text

---
+++ lib/widgets/w_rounded_text.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:nft/services/safety/base_stateless.dart';
+import 'package:nft/utils/app_extension.dart';
+import 'package:nft/utils/app_style.dart';
+
+// ignore: must_be_immutable
+class WRoundedText extends BaseStateless {
+  WRoundedText({Key key, this.text, this.color}) : super(key: key);
+
+  final String text;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Container(
+      padding: EdgeInsets.all(2.W),
+      margin: EdgeInsets.symmetric(horizontal: 5.W),
+      decoration: BoxDecoration(
+        border: Border.all(color: color),
+        borderRadius: BorderRadius.circular(10.W),
+      ),
+      child: Text(
+        text ?? '',
+        textAlign: TextAlign.center,
+        style: boldTextStyle(15.SP, color: color),
+      ),
+    );
+  }
+}
