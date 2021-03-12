fb34503764577de2b3e17de335eb62b4f6ac677f
Nhan Cao <nhancv92@gmail.com>
2021-03-12 09:06:23 +0700

    feat: update ci config

--- _ci/bitrise-apk-download-urls.sh
+++ _ci/bitrise-apk-download-urls.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-#BITRISE_PERMANENT_DOWNLOAD_URL_MAP='app-universal-release.apk=>https://app.bitrise.io/artifact/98e1ae554fde01b4/download|app-release.aab=>https://app.bitrise.io/artifact/5f2b684b4cccdfac/download|flutter_json_test_results.json=>https://app.bitrise.io/artifact/5e8ada02e18e8700/download'
+#BITRISE_PERMANENT_DOWNLOAD_URL_MAP='app-universal-release.apk=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download|app-release.aab=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download|flutter_json_test_results.json=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download'
 
 BITRISE_APK_URL=""
 BITRISE_AAB_URL=""
-APK_FILE=app-universal-release.apk
-AAB_FILE=app-release.aab
+APK_FILE='app-universal-release.apk'
+AAB_FILE='app-release.aab'
 IFS='|' read -ra URL <<< "$BITRISE_PERMANENT_DOWNLOAD_URL_MAP"
 for i in "${URL[@]}"; do
   if [[ $i == $APK_FILE* ]];
---
+++ _ci/bitrise-ipa-download-urls.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+BITRISE_PERMANENT_DOWNLOAD_URL_MAP='export_options.plist=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download|flutter_json_test_results.json=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download|Runner.app.zip=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download|Runner.dSYM.zip=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download|Runner.ipa=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download|Runner.xcarchive.zip=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download'
+
+BITRISE_IPA_URL=""
+IPA_FILE='Runner.ipa'
+IFS='|' read -ra URL <<< "$BITRISE_PERMANENT_DOWNLOAD_URL_MAP"
+for i in "${URL[@]}"; do
+  if [[ $i == $IPA_FILE* ]];
+	then
+		BITRISE_IPA_URL=$(echo $i | sed "s/${IPA_FILE}=>//")
+	fi
+
+done
+
+echo "BITRISE_IPA_URL: $BITRISE_IPA_URL"
+envman add --key BITRISE_IPA_URL --value "$BITRISE_IPA_URL"
