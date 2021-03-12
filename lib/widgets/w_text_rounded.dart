c19d1e042780278d0c7cc4aaf865e4883e7d9f00
Nhan Cao <nhancv92@gmail.com>
2021-03-12 09:51:59 +0700

    feat: update text rouned widget

--- lib/widgets/w_text_rounded.dart
+++ lib/widgets/w_text_rounded.dart
@@ -5,27 +5,36 @@ import 'package:nft/utils/app_style.dart';
 
 // ignore: must_be_immutable
 class WTextRounded extends BaseStateless {
-  WTextRounded({Key key, this.text, this.color, this.background}) : super(key: key);
+  WTextRounded({
+    Key key,
+    this.text,
+    this.color,
+    this.background,
+    this.textStyle,
+  }) : super(key: key);
 
   final String text;
   final Color color;
   final Color background;
+  final TextStyle textStyle;
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
     return Container(
       padding: EdgeInsets.all(2.W),
-      margin: EdgeInsets.symmetric(horizontal: 5.W),
+      margin: EdgeInsets.symmetric(horizontal: 5.W, vertical: 5.W),
       decoration: BoxDecoration(
         color: background,
-        border: Border.all(color: color),
+        border: Border.all(
+          color: color ?? textStyle?.color ?? Colors.transparent,
+        ),
         borderRadius: BorderRadius.circular(10.W),
       ),
       child: Text(
         text ?? '',
         textAlign: TextAlign.center,
-        style: boldTextStyle(15.SP, color: color),
+        style: textStyle ?? boldTextStyle(15.SP, color: color),
       ),
     );
   }
