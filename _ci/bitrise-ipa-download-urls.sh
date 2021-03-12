d7a2f8de5ce733c652ec46ed2898f7597fc2c540
Nhan Cao <nhancv92@gmail.com>
2021-03-12 12:05:35 +0700

    feat: correct ios download url

--- _ci/bitrise-ipa-download-urls.sh
+++ _ci/bitrise-ipa-download-urls.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-BITRISE_PERMANENT_DOWNLOAD_URL_MAP='export_options.plist=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download|flutter_json_test_results.json=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download|Runner.app.zip=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download|Runner.dSYM.zip=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download|Runner.ipa=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download|Runner.xcarchive.zip=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download'
+#BITRISE_PERMANENT_DOWNLOAD_URL_MAP='export_options.plist=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download|flutter_json_test_results.json=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download|Runner.app.zip=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download|Runner.dSYM.zip=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download|Runner.ipa=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download|Runner.xcarchive.zip=>https://app.bitrise.io/artifact/nhancv_depchai_xxxxx/download'
 
 BITRISE_IPA_URL=""
 IPA_FILE='Runner.ipa'
